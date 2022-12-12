a78fe1a1ff2f70934bdee9698482c0a642ca42f0
Stanislav Finko <ghostzip8@gmail.com>
2022-12-12 18:42:03 +0200

    Create custome user model. Make migrations.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     # apps:
-    "src.core"
+    "src.core",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
