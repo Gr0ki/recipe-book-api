74d97bd6915326da33b490eabedce5b376653d09
Stanislav Finko <ghostzip8@gmail.com>
2022-12-12 19:07:21 +0200

    Add custom User normalization.

--- backend/src/core/tests/test_models.py
+++ backend/src/core/tests/test_models.py
@@ -19,3 +19,17 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ["  test1@example.com       ", "test1@example.com"],
+            ["     TEST2@EXAMPLE.com   ", "TEST2@example.com"],
+            ["test3@EXAMPLE.com", "test3@example.com"],
+            ["Test4@Example.com", "Test4@example.com"],
+            ["TEST5@EXAMPLE.com", "TEST5@example.com"],
+            ["test6@example.COM", "test6@example.com"],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "sample123")
+            self.assertEqual(user.email, expected)
