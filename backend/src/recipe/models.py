73582a6fd30e90fcd84ea536935cae637d1e52da
Stanislav Finko <ghostzip8@gmail.com>
2022-12-26 00:18:08 +0200

    Add Recipe model, add new model to the admin page.

--- backend/src/recipe/models.py
+++ backend/src/recipe/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    IntegerField,
+    CharField,
+    TextField,
+    ForeignKey,
+    DecimalField,
+    SET_NULL,
+)
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Recipe(Model):
+    user = ForeignKey(get_user_model(), null=True, on_delete=SET_NULL)
+    title = CharField(max_length=40)
+    time_minutes = IntegerField()
+    price = DecimalField(decimal_places=2, max_digits=8)
+    description = TextField()
+    link = CharField(max_length=255, blank=True)
---
+++ backend/src/recipe/tests/test_modules.py
@@ -0,0 +1,47 @@
+"""
+Tests for models.
+"""
+from decimal import Decimal
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from ..models import Recipe
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def setUp(self):
+        """Creates user for all tests."""
+        email = "test@example.com"
+        password = "testpass123S"
+        self.user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+    def test_create_recipe_successful(self):
+        """Test creating a recipe successful."""
+        recipe = Recipe.objects.create(
+            user=self.user,
+            title="Test recipe name.",
+            time_minutes=5,
+            price=Decimal("200.34"),
+            description="Test description.",
+        )
+        recipes_query = Recipe.objects.all()
+        self.assertIn(recipe, recipes_query)
+        self.assertEqual(len(recipes_query), 1)
+        self.assertEqual(recipes_query.get(user=self.user).user, self.user)
+
+    def test_create_recipe_fail(self):
+        """Test creating a recipe fail."""
+        recipe = Recipe.objects.create(
+            title="Test recipe name.",
+            time_minutes=5,
+            price=Decimal("200.34"),
+            description="Test description.",
+        )
+        recipes_query = Recipe.objects.all()
+        self.assertEqual(len(recipes_query), 0)
